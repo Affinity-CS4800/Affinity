3e09ff3d33d009e0020e65f4dd58c4db81486650
Bryce Callender <brycec848@gmail.com>
2019-09-20 17:49:48 -0700

    Added Graph Controller and Views

---
+++ Affinity/Controllers/GraphController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Affinity.Controllers
+{
+    public class GraphController : Controller
+    {
+        [Route("/graph")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/graph/{id}")]
+        public IActionResult GetSpecificGraph()
+        {
+            return Json(new { id = "1", value = "GetSpecificGraph" });
+        }
+
+        [Route("/graphs")]
+        public IActionResult GetGraphs()
+        {
+            return Json(new { id = "2", value = "GetGraphs" });
+        }
+    }
+}

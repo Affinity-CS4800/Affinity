4c01e3aa0fb7ec95967654ae94ae0a43437483ba
Jordan Laidig <jrlaidig@gmail.com>
2019-09-20 17:22:49 -0700

    added pages for login and register

--- Affinity/Controllers/AffinityController.cs
+++ Affinity/Controllers/AffinityController.cs
@@ -15,10 +15,29 @@ namespace Affinity.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [Route("/about")]
+        public IActionResult About()
         {
             return View();
         }
+
+        [Route("/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [Route("/register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [Route("/api/testjson")]
+        public IActionResult TestJson()
+        {
+            return Json(new { id = 1, value = "hello" });
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

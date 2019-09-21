53d5290b2a198e3aaf649f41bd1c70624d731d01
Bryce <brycec848@gmail.com>
2019-09-21 14:56:30 -0700

    Added Vertex and Edge objects based on doc

---
+++ Affinity/Models/Graph.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+public enum Direction
+{
+    Undirected,
+    DirectedAtFirst,
+    DirectedAtSecond
+}
+
+namespace Affinity.Models
+{
+    public class Vertex
+    {
+        public int ID { get; set; }
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public int Color { get; set; }
+        [StringLength(8)]
+        public string Name { get; set; }
+        public List<Edge> Edges { get; set; }
+    }
+
+    public class Edge
+    {
+        public int ID { get; set; }
+        public int First { get; set; }
+        public int Second { get; set; }
+        [EnumDataType(typeof(Direction))]
+        public Direction Direction { get; set; }
+        public int Color { get; set; }
+        [Range(-1,2000)]
+        public int Weight { get; set; }
+    }
+
+    public static class GraphConstants
+    {
+        public static int GRID_MAX = 3000;
+        public static int VERTEX_MAX = 3201;
+        public static int EDGE_MAX = 3200;
+    }
+}

681a77d9633ea86c060224e78864e0accd61a0e1
Bryce Callender <brycec848@gmail.com>
2019-09-22 14:40:01 -0700

    Added NewtonSoft for JSON library and made a test func

--- Affinity/Controllers/GraphController.cs
+++ Affinity/Controllers/GraphController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Affinity.Models;
+using System.Drawing;
 
 namespace Affinity.Controllers
 {
@@ -24,6 +27,38 @@ namespace Affinity.Controllers
         public IActionResult GetGraphs()
         {
             return Json(new { id = "2", value = "GetGraphs" });
+        }
+
+        [Route("/api/test")]
+        public string TestNewtonSoft()
+        {
+            Vertex vertex = new Vertex();
+
+            vertex.Name = "A";
+            vertex.XPos = 200;
+            vertex.YPos = 300;
+            vertex.Color = Color.Aqua.ToArgb();
+            vertex.Edges = new List<Edge>
+            {
+                new Edge
+                {
+                    ID = 1,
+                    Weight = 8,
+                    First = 1,
+                    Second = 2,
+                    Color = Color.Black.ToArgb()
+                },
+                new Edge
+                {
+                    ID = 2,
+                    Weight = 3,
+                    First = 2,
+                    Second = 1,
+                    Color = Color.Black.ToArgb()
+                }
+            };
+
+            return JsonConvert.SerializeObject(vertex, Formatting.Indented);
         }
     }
 }

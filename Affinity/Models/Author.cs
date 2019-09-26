666b5d78f3a255910917ebd5a82d105c5264390e
Jordan Laidig <jrlaidig@gmail.com>
2019-09-25 18:11:53 -0700

    added portion for assignment 4

---
+++ Affinity/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Affinity.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(75)]
+        public string LastName { get; set; }
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ Affinity/Models/EFCoreWebDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Affinity.Models
+{
+    public class EFCoreWebDemoContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //needs to add a data base for code to work
+            optionsBuilder.UseSqlServer(@"Server=.\;Database=INSERT DATABASE HERE;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
